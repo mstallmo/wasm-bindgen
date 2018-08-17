2696ec9d0bdcc9e089f7fe023511d75fb92183f8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-17 15:48:56 +0000

    Deploy rustwasm/wasm-bindgen to github.com/rustwasm/wasm-bindgen.git:gh-pages

---
+++ api/web_sys/css/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["escape","The `CSS.escape()` function"],["supports_with_condition_text","The `CSS.supports()` function"],["supports_with_property_and_value","The `CSS.supports()` function"]]});
